091b8956aed457b33a39a4f6aed9b4e0a39eae6f
mani <manis.tinku123@gmail.com>
2021-10-21 18:35:03 +0530

    change ami

--- 02/c2-ec2-instance.tf
+++ 02/c2-ec2-instance.tf
@@ -1,6 +1,6 @@
 
 resource "aws_instance" "MY-VM" {
-  ami = "ami-02e136e904f3da870"
+  ami = "ami-041d6256ed0f2061c"
   instance_type = "t2.micro"
   user_data = file("${path.module}/sampleapp1.sh")
   tags = {
