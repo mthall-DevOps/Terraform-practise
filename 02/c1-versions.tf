f1d447260acf434c7a3da1ade823d7f465084a8c
mani <manis.tinku123@gmail.com>
2021-10-21 18:24:07 +0530

    02 change

--- 02/c1-versions.tf
+++ 02/c1-versions.tf
@@ -11,12 +11,3 @@ provider "aws" {
   profile = "default"
   region = "us-east-1"
 }
-
-resource "aws_instance" "MY-VM" {
-  ami = "ami-02e136e904f3da870"
-  instance_type = t2.micro
-  user_data = file("${path.module}/sampleapp1.sh")
-  tags = {
-    "Name" = "DEMOVM"
-  }
-}
