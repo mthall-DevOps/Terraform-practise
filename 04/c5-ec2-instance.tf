f1682dc01b808cf7e9489b25f6f83b897602d38e
mani <manis.tinku123@gmail.com>
2021-10-26 16:37:26 +0530

    changes in C5

--- 04/c5-ec2-instance.tf
+++ 04/c5-ec2-instance.tf
@@ -1,9 +1,9 @@
 
 resource "aws_instance" "MY-VM" {
   ami = data.aws_ami.amzlinux2.id 
-  #instance_type = var.instance_type
+  instance_type = var.instance_type
   #instance_type = var.instance_type_list[0]
-  instance_type = var.instance_type_map["qa"]
+  #instance_type = var.instance_type_map["qa"]
   user_data = file("${path.module}/sampleapp1.sh")
   key_name = var.aws_keypair
   vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id ]
