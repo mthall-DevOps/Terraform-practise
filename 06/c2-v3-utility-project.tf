e1093ded652556e220e4430e82e3e537bede1658
mani <manis.tinku123@gmail.com>
2021-10-27 16:23:05 +0530

    new v3 file

---
+++ 06/c2-v3-utility-project.tf
@@ -0,0 +1,35 @@
+#list of AZ's
+data "aws_availability_zones" "azones" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+# Datasource
+data "aws_ec2_instance_type_offerings" "my_ins_type" {
+  for_each = toset(data.aws_availability_zones.azones.names)
+  filter {
+    name   = "instance-type"
+    values = ["t2.micro"]
+  }
+  filter {
+    name   = "location"
+    values = [each.key] 
+  }
+  location_type = "availability-zone"
+}
+
+
+# Output
+output "output_v2_1" {
+ #value = data.aws_ec2_instance_type_offerings.my_ins_type1.instance_types
+ value = toset([ for t in data.aws_ec2_instance_type_offerings.my_ins_type2: t.instance_types])
+}
+
+output "output_v2_2" {
+ #value = data.aws_ec2_instance_type_offerings.my_ins_type1.instance_types
+ #value = toset([ for t in data.aws_ec2_instance_type_offerings.my_ins_type2: t.instance_types])
+ value = {
+   for az, i in data.aws_ec2_instance_type_offerings.my_ins_type2: az => i.instance_types
+ }
+}
