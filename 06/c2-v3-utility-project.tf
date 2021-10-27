38b6e58adba8017badb81fabbf8c04c250ab5c9a
mani <manis.tinku123@gmail.com>
2021-10-27 16:39:11 +0530

    new v3 file

--- 06/c2-v3-utility-project.tf
+++ 06/c2-v3-utility-project.tf
@@ -33,14 +33,14 @@ value = {
  }
 }
 #take only keys
-output "output_v3_2" {
+output "output_v3_3" {
 value = keys({
    for az, i in data.aws_ec2_instance_type_offerings.my_ins_type: 
    az => i.instance_types if length(i.instance_types) != 0
  })
 }
 #only one value from the list of keys
-output "output_v3_2" {
+output "output_v3_4" {
 value = keys({
    for az, i in data.aws_ec2_instance_type_offerings.my_ins_type: 
    az => i.instance_types if length(i.instance_types) != 0
