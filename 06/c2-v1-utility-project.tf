28a9452ce7c41effc4b23b5a31881faa9dce57a9
mani <manis.tinku123@gmail.com>
2021-10-27 15:51:33 +0530

    adding 06 demo

---
+++ 06/c2-v1-utility-project.tf
@@ -0,0 +1,19 @@
+# Datasource
+data "aws_ec2_instance_type_offerings" "my_ins_type1" {
+  filter {
+    name   = "instance-type"
+    values = ["t2.micro"]
+  }
+  filter {
+    name   = "location"
+    values = ["ap-south-1a"]
+    #values = ["ap-south-1c"]   
+  }
+  location_type = "availability-zone"
+}
+
+
+# Output
+output "output_v1_1" {
+ value = data.aws_ec2_instance_type_offerings.my_ins_type1.instance_types
+}
