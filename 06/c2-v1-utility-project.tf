c2ed11b93d602c75687b34b33781784f94b7bde4
mani <manis.tinku123@gmail.com>
2021-10-27 15:56:13 +0530

    change 06 demo

--- 06/c2-v1-utility-project.tf
+++ 06/c2-v1-utility-project.tf
@@ -6,8 +6,8 @@ data "aws_ec2_instance_type_offerings" "my_ins_type1" {
   }
   filter {
     name   = "location"
-    values = ["ap-south-1a"]
-    #values = ["ap-south-1c"]   
+    #values = ["ap-south-1a"]
+    values = ["ap-south-1c"]   
   }
   location_type = "availability-zone"
 }
