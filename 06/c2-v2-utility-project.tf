127153edf58695ad382abc794606d8b2b4b73f3c
mani <manis.tinku123@gmail.com>
2021-10-27 16:04:52 +0530

    new v2 file

--- 06/c2-v2-utility-project.tf
+++ 06/c2-v2-utility-project.tf
@@ -14,7 +14,7 @@ data "aws_ec2_instance_type_offerings" "my_ins_type2" {
 
 
 # Output
-output "output_v1_1" {
+output "output_v2_1" {
  #value = data.aws_ec2_instance_type_offerings.my_ins_type1.instance_types
  value = [ for t in data.aws_ec2_instance_type_offerings.my_ins_type2: t.instance_types] 
 }
